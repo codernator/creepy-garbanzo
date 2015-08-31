3207afb52ee49e2b7a45b243ee07c713415d0b25
codernator <brandon.l.griffin@gmail.com>
2015-08-31 09:08:47 -0400

    Change make order to compile files then splint them. Fixed logging.c according to splint.

--- code/src/logging.c
+++ code/src/logging.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
-
+#include <stdlib.h>
 
 static void write_file(const char *fileName, char *mode, const char *fmt, ...);
 
@@ -15,31 +15,32 @@ void append_file(CHAR_DATA *ch, const char *file, const char *str)
 
     DENY_NPC(ch);
     if (str == NULL || str[0] == '\0')
-	return;
+        return;
 
     if ((fp = fopen(file, "a")) == NULL) {
-	perror(file);
-	send_to_char("Sorry, the needed file is inaccessible.", ch);
-	return;
+        perror(file);
+        send_to_char("Sorry, the needed file is inaccessible.", ch);
+        return;
     }
 
     fprintf(fp, "[%5ld] %s: %s\n", ch->in_room == NULL ? 0 : ch->in_room->vnum, ch->name, str);
-    fclose(fp);
+
+    if (fclose(fp) != 0) {
+        perror(file);
+        ABORT;
+    }
 }
 
 void log_bug(const char *fmt, ...)
 {
     static char buf[MSL];
-    char *strtime;
     va_list args;
 
     va_start(args, fmt);
-    vsprintf(buf, fmt, args);
+    (void)vsnprintf(buf, MSL, fmt, args);
     va_end(args);
 
-    strtime = ctime(&globalSystemState.current_time);
-    strtime[strlen(strtime) - 1] = '\0';
-    fprintf(stderr, "%s :: %s\n", strtime, buf);
+    fprintf(stderr, "%s :: %s\n", ctime(&globalSystemState.current_time), buf);
 }
 
 /*
@@ -48,53 +49,46 @@ void log_bug(const char *fmt, ...)
 void log_string(const char *fmt, ...)
 {
     static char buf[MSL];
-    char *strtime;
     va_list args;
 
     va_start(args, fmt);
-    vsprintf(buf, fmt, args);
+    (void)vsnprintf(buf, MSL, fmt, args);
     va_end(args);
 
-    strtime = ctime(&globalSystemState.current_time);
-    strtime[strlen(strtime) - 1] = '\0';
-    fprintf(stderr, "%s :: %s\n", strtime, buf);
+    fprintf(stderr, "%s :: %s\n", ctime(&globalSystemState.current_time), buf);
 }
 
 
 void log_to(int log, char username[], const char *fmt, ...)
 {
-    char *strtime;
     static char buf[MSL];
     va_list args;
 
     va_start(args, fmt);
-    vsprintf(buf, fmt, args);
+    (void)vsnprintf(buf, MSL, fmt, args);
     va_end(args);
-
-    strtime = ctime(&globalSystemState.current_time);
-    strtime[strlen(strtime) - 1] = '\0';
 
     switch (log)
     {
-	case LOG_SINK_LASTCMD:
-	    write_file(LAST_COMMANDS, "a+", "[%s] %s\n", strtime, buf);
-	    break;
-	case LOG_SINK_ALWAYS:
-	case LOG_SINK_ALL:
-	    write_file(LOG_ALWAYS_FILE, "a+", "[%s] %s\n", strtime, buf);
-	    break;
-	case LOG_SINK_PLAYER:
-	    if (username[0] == '\0') {
-		log_bug("ERROR- log_to: Log Type 'Player' requires a username!");
-	    } else {
-		static char log_file_name[2*MIL];
-		(void)snprintf(log_file_name, 2*MIL, LOG_PLAYER_FILE, username);
-		write_file(log_file_name, "a+", "[%s] %s\n", strtime, buf);
-	    }
-	    break;
-	default:
-	    log_bug("ERROR - log_to: Log type '%d' does not exist.", log);
-	    break;
+        case LOG_SINK_LASTCMD:
+            write_file(LAST_COMMANDS, "a+", "[%s] %s\n", ctime(&globalSystemState.current_time), buf);
+            break;
+        case LOG_SINK_ALWAYS:
+        case LOG_SINK_ALL:
+            write_file(LOG_ALWAYS_FILE, "a+", "[%s] %s\n", ctime(&globalSystemState.current_time), buf);
+            break;
+        case LOG_SINK_PLAYER:
+            if (username[0] == '\0') {
+                log_bug("ERROR- log_to: Log Type 'Player' requires a username!");
+            } else {
+                static char log_file_name[2*MIL];
+                (void)snprintf(log_file_name, 2*MIL, LOG_PLAYER_FILE, username);
+                write_file(log_file_name, "a+", "[%s] %s\n", ctime(&globalSystemState.current_time), buf);
+            }
+            break;
+        default:
+            log_bug("ERROR - log_to: Log type '%d' does not exist.", log);
+            break;
     }
 }
 
@@ -105,15 +99,19 @@ static void write_file(const char *fileName, char *mode, const char *fmt, ...)
     va_list args;
 
     va_start(args, fmt);
-    vsprintf(buf, fmt, args);
+    (void)vsnprintf(buf, MSL, fmt, args);
     va_end(args);
 
     if ((thisFile = fopen(fileName, mode)) == NULL) {
-	log_bug("Error - log_to - cannot open file '%s' in mode %s", fileName, mode);
-	return;
+        perror(fileName);
+        ABORT;
     }
 
     fprintf(thisFile, "%s", buf);
-    fclose(thisFile);
+
+    if (fclose(thisFile) != 0) {
+        perror(fileName);
+        ABORT;
+    }
 }
 
